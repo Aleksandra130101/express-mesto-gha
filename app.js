2f70d02f9f7b2a7462adef782eb7c5db8698bb31
Aleksandra130101 <104098142+Aleksandra130101@users.noreply.github.com>
2023-04-28 20:05:33 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -33,7 +33,7 @@ app.post('/signup', celebrate({
   body: Joi.object().keys({
     name: Joi.string().min(2).max(30),
     about: Joi.string().min(2).max(30),
-    avatar: Joi.string().pattern(urlRegExp),
+    avatar: Joi.string().uri({ scheme: ["http", "https"] }),
     email: Joi.string().required().email(),
     password: Joi.string().required().min(8),
   }),
